ab1ea69e3ec4982c52ab939d66a179db8d8a9823
Repository-Dio-sole <minasoliman68@gmail.com>
2025-03-24 13:13:45 +0100

    grafici2

--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -3,10 +3,10 @@ from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 #oggi leak di chiavi api
 sp_oauth = SpotifyOAuth(
     client_id='c1e1e455737c44a5a404101c638179d8',
-    client_secret='e4d8a63e280e4fe5aa5cb455555c6808',
+    client_secret='5197209f857f4eee8537c515723e89d7',
     redirect_uri='http://127.0.0.1:5000/callback',  
 
-    scope="user-read-private", #permessi x informazioni dell'utente
+    scope="user-read-private user-read-email playlist-read-private", #permessi x informazioni dell'utente
     show_dialog=True
 )
 
@@ -18,4 +18,4 @@ def get_spotify_object(token_info):
     return spotipy.Spotify(auth=token_info['access_token']) 
 
 def get_credentials():
-    return SpotifyClientCredentials(client_id='c1e1e455737c44a5a404101c638179d8',client_secret='e4d8a63e280e4fe5aa5cb455555c6808')
+    return SpotifyClientCredentials(client_id='c1e1e455737c44a5a404101c638179d8',client_secret='5197209f857f4eee8537c515723e89d7')
