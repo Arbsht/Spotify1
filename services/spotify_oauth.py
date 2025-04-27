28b4ef230787f2fff09d7f896e1dd53e42b7b816
alessandro pantazi <pantazi.george@iisgalvanimi.edu.it>
2025-04-27 11:29:47 +0000

    grafica

--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -2,8 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 #oggi leak di chiavi api
 sp_oauth = SpotifyOAuth(
-    client_id='486292e9ce954bab9887cd9ffba68664',
-    client_secret='32d2857211c74100b0186990146416f2',
+    client_id='9f1ea3fb16f04a4fb1aaa3d4e87bc179',
+    client_secret='ed3e093fc6ff4108a60ffd6a6cd521e2',
     redirect_uri='http://127.0.0.1:5000/callback',  
 
     scope="user-read-private user-read-email playlist-read-private", #permessi x informazioni dell'utente
@@ -14,5 +14,5 @@ def get_spotify_object(token_info):
     return spotipy.Spotify(auth=token_info['access_token']) 
 
 def get_credentials():
-    return SpotifyClientCredentials(client_id='486292e9ce954bab9887cd9ffba68664',client_secret='32d2857211c74100b0186990146416f2')
+    return SpotifyClientCredentials(client_id='9f1ea3fb16f04a4fb1aaa3d4e87bc179',client_secret='ed3e093fc6ff4108a60ffd6a6cd521e2')
     
